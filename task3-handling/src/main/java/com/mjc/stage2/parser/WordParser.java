e0bda429cb1ce9b689561a722da1dd23d3c11c88
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-13 21:54:56 +0300

    Update WordParser.java

--- task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/WordParser.java
@@ -1,8 +1,27 @@
 package com.mjc.stage2.parser;
 
+import com.mjc.stage2.entity.AbstractTextComponent;
+import com.mjc.stage2.entity.TextComponent;
+import com.mjc.stage2.entity.TextComponentType;
 
-public class WordParser {
+import java.util.ArrayList;
+import java.util.List;
 
-    // Write your code here!
+public class WordParser extends AbstractTextParser{
+    @Override
+    public void parse(AbstractTextComponent abstractTextComponent, String string) {
+        String[] str = string.split("\\s+");
+        List<Character> chars2 = new ArrayList<>();
+        for (String x: str) {
+            char[] chars = x.toCharArray();
+            for (int i = 0; i < chars.length ; i++) {
+                chars2.add(chars[i]);
+            }
+        }
+        Character[] newChars = chars2.stream().toArray(Character[]::new);
 
+        for (char charX : newChars) {
+            abstractTextComponent.add(new SymbolLeaf(TextComponentType.WORD, charX));
+        }
+    }
 }
