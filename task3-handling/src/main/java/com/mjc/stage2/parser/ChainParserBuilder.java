776ef28053212012a76bb17ad4b9101e08f82b7b
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-07-13 21:52:26 +0300

    Update ChainParserBuilder.java

--- task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
+++ task3-handling/src/main/java/com/mjc/stage2/parser/ChainParserBuilder.java
@@ -10,12 +10,17 @@ public class ChainParserBuilder {
     }
 
     public ChainParserBuilder setParser(AbstractTextParser abstractTextParser) {
-        // Write your code here!
+        parsers.add(abstractTextParser);
         return this;
     }
-
     public AbstractTextParser build() {
-        // Write your code here!
-        return null;
+        AbstractTextParser abstractTextParser = null;
+
+        for (int i = 0; i < parsers.size() - 1; i++) {
+            abstractTextParser = parsers.get(i);
+            AbstractTextParser nextParser = parsers.get(i + 1);
+            abstractTextParser.setNextParser(nextParser);
+        }
+        return abstractTextParser;
     }
 }
